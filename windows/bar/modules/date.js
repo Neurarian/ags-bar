26df616f0f53b07377d71e2fbb016b5a011cff29
Neurarian <110474238+Neurarian@users.noreply.github.com>
2024-11-03 15:45:12 +0100

    add: power button revealer to clock

--- windows/bar/modules/date.js
+++ windows/bar/modules/date.js
@@ -1,13 +1,41 @@
-import { Utils, Widget } from "../../../imports.js";
+
+import { Utils, Widget, Icons } from "../../../imports.js";
 
 export default () =>
   Widget.EventBox({
-    child: Widget.Label({ className: "date module" })
-      .poll(
-        1000,
-        (self) =>
-          Utils.execAsync(["date", "+%H 󰇙 %M"]).then((r) =>
-            self.label = r
-          ),
-      ),
+    // Toggle the reveal state of the Revealer on hover in and out
+    onHover: (self) => {
+      self.get_child().children[1].revealChild = true;
+    },
+    onHoverLost: (self) => {
+      self.get_child().children[1].revealChild = false;
+    },
+    child: Widget.Box({
+      children: [
+        Widget.Label({
+          className: "date-module",
+          setup: (self) =>
+            self.poll(1000, () =>
+              Utils.execAsync(["date", "+%H 󰇙 %M"]).then(
+                (r) => (self.label = r),
+              ),
+            ),
+        }),
+        // Reveal Power Button on hover
+        Widget.Revealer({
+          revealChild: false,
+          transitionDuration: 300,
+          transition: "slide_right",
+          child: Widget.Button({
+            className: "power-button",
+            onClicked: () => Utils.execAsync(["shutdown --poweroff"]),
+            // Comment above line and uncomment below to use wlogout instead
+            // onClicked: () => Utils.execAsync(["wlogout"]),
+            child: Widget.Icon({
+              icon: Icons.powerButton,
+            }),
+          }),
+        }),
+      ],
+    }),
   });
