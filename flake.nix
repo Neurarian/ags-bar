765392c69517cdb4d4b49b54022507a2c49711ec
Neurarian <110474238+Neurarian@users.noreply.github.com>
2025-03-05 23:00:20 +0100

    flake: always use most recent astal version

--- flake.nix
+++ flake.nix
@@ -9,6 +9,11 @@
     };
     ags = {
       url = "github:aylur/ags";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.astal = {
+        url = "github:aylur/astal";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
     };
   };
 
