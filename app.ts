a0425aa1524a54c9135b08033915d54735db534e
Neurarian <110474238+Neurarian@users.noreply.github.com>
2025-03-05 08:26:55 +0100

    fix: build fail on nix
    streamline application of css

--- app.ts
+++ app.ts
@@ -1,30 +1,26 @@
 import { App, Gdk, Gtk } from "astal/gtk3"
 import { monitorFile } from "astal/file"
-import { exec } from "astal/process"
 import Bar from "./widgets/bar/main.tsx"
 import OnScreenDisplay from "./widgets/osd/main.tsx"
 import Notifications from "./widgets/notifications/popups.tsx";
 import SystemMenu from "./widgets/system-menu/main.tsx"
 import MusicPlayer from "./widgets/music/main.tsx"
-const css = "./style.css";
-const scss = "./style.scss";
+import scss from "./style.scss";
 
 function reloadCss() {
     console.log("scss change detected");
-    exec(`sass ${scss} ${css}`);
-    App.apply_css(css);
+    App.apply_css(scss);
 }
 
 App.start({
     icons: "./icons",
-    css: css,
+    css: scss,
     instanceName: "js",
     requestHandler(request, res) {
         print(request)
         res("ok")
     },
     main() {
-        exec(`sass ${scss} ${css}`);
         monitorFile(`./style`, reloadCss);
         const bars = new Map<Gdk.Monitor, Gtk.Widget>()
 
--- flake.nix
+++ flake.nix
@@ -7,10 +7,6 @@
     systems = {
       url = "systems";
     };
-    astal = {
-      url = "github:aylur/astal?rev=bfa376a3468f7c657e46a87c61ea7e9d8c6336b4";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     ags = {
       url = "github:aylur/ags";
       inputs.astal.url = "github:aylur/astal?rev=bfa376a3468f7c657e46a87c61ea7e9d8c6336b4";
@@ -20,7 +16,6 @@
   outputs = {
     self,
     nixpkgs,
-    astal,
     flake-parts,
     systems,
     ags,
@@ -37,42 +32,36 @@
         pkgs = mkPkgs system;
         name = "matshell";
       in {
-        packages.default = pkgs.stdenvNoCC.mkDerivation {
+        packages.default = ags.lib.bundle {
+          inherit pkgs name;
           src = ./.;
-          inherit name;
           entry = "app.ts";
 
-          nativeBuildInputs = with pkgs; [
-            ags.packages.${system}.default
-            wrapGAppsHook
-            gobject-introspection
-            typescript
-            dart-sass
-            mission-center
-            imagemagick_light
-          ];
-
-          buildInputs = with astal.packages.${system}; [
-            notifd
-            hyprland
-            wireplumber
-            mpris
-            network
-            tray
-            bluetooth
-            cava
-            # any other package
-          ];
-
-          installPhase = ''
-            mkdir -p $out/bin
-            ags bundle app.ts $out/bin/${name}
-            chmod +x $out/bin/${name}
-          '';
+          extraPackages =
+            (with ags.packages.${system}; [
+              io
+              notifd
+              hyprland
+              wireplumber
+              mpris
+              network
+              tray
+              bluetooth
+              cava
+            ])
+            ++ (with pkgs; [
+              ags.packages.${system}.default
+              wrapGAppsHook
+              gobject-introspection
+              typescript
+              dart-sass
+              mission-center
+              imagemagick_light
+              # any other package
+            ]);
         };
 
         devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with self.packages.${system}; default.nativeBuildInputs;
           inputsFrom = builtins.attrValues {
             inherit (self.packages.${system}) default;
           };
